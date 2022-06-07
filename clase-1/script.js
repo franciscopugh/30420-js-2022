6cfc8b9131baa579d9211dcb1cfaa26c3596d1d0
Francisco Pugh <franciscopugh01@gmail.com>
2022-06-06 23:39:16 -0300

    Clase 1

---
+++ clase-1/script.js
@@ -0,0 +1,38 @@
+//hola Hola
+/*
+    Comentario multi-linea
+*/
+/**
+ * 
+
+
+var nombre = 'Francisco' //String
+var apellido = "Pugh"
+var nombreApellido = "Francisco Pugh"
+var nombre_apellido = "Francisco Pugh"
+
+var edad = 37 //Number
+var esProfesor = true //Boolean
+var sueldo = null //null (vacio)
+var precioArroz = undefined //undefined
+var precio
+
+console.log(nombreApellido)
+//alert(esProfesor)
+ */
+
+const IVA = 1.21
+
+var nombre_apellido = "Francisco Pugh"
+let nombreApellido = "Francisco Pugh"
+
+var nombre_apellido = undefined
+
+//IVA = 1.10
+
+nombreApellido = 15
+
+console.log(100 * IVA)
+//console.log(nombre_apellido)
+
+console.log(nombreApellido)
